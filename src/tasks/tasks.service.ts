e1a2c4fecb6c706279764c9c356a31a64621338b
Shahanul Haque Shawon <37122193+shawon1fb@users.noreply.github.com>
2021-09-03 23:14:32 +0600

    get all task

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string) {
+    const task: Task = {
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+  }
+}
