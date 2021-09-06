9e4ff868072e4bcaa90ede367debfdc6d3103942
Shahanul Haque Shawon <37122193+shawon1fb@users.noreply.github.com>
2021-09-06 23:06:56 +0600

    make drop db 1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Logger } from "@nestjs/common";
 
 async function bootstrap() {
+  const logger = new Logger("bootstrap");
   const app = await NestFactory.create(AppModule);
+  const port = 8080;
   await app.listen(8080);
+  logger.log(`Application listening on port ${port}`);
 }
+
 bootstrap();
--- src/tasks/task-repository.ts
+++ src/tasks/task-repository.ts
@@ -3,24 +3,29 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task-entity';
 import { TaskStatus } from './task-status.enum';
 import { GetTaskFilterDto } from './dto/get-task-filter.dto';
+import { User } from '../auth/user.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto;
     const task = new Task();
 
     task.title = title;
     task.description = description;
     task.status = TaskStatus.OPEN;
+    task.user = user;
 
     await task.save();
+    delete task.user;
     return task;
   }
 
-  async getTasks(filterDto: GetTaskFilterDto): Promise<Task[]> {
+  async getTasks(filterDto: GetTaskFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder('task');
+
+    query.where('task.userId=:userId', { userId: user.id });
 
     if (status) {
       query.andWhere('task.status = :status', { status });
