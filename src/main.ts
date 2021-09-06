24cac5f2e4bb01d681c0ee93f9c6f4bda51273ca
Shahanul Haque Shawon <37122193+shawon1fb@users.noreply.github.com>
2021-09-06 23:08:11 +0600

    make drop db 2

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
-import { Logger } from "@nestjs/common";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const logger = new Logger("bootstrap");
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   const port = 8080;
   await app.listen(8080);
